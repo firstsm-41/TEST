c165d7295a6ed4fb3b44bd3cf777408b14599aa4
Sungmin <firstsm41@naver.com>
2024-08-02 18:45:55 +0900

    addfile 두번째 커밋

---
+++ src/yongbin.java
@@ -0,0 +1,13 @@
+public class yongbin {
+
+  public static void main(String[] args) {
+    System.out.println("용용");
+    System.out.println("dnfn");
+    System.out.println("dnfn");
+    System.out.println("dnfn");
+    System.out.println("dnadfdvfn");
+
+
+  }
+
+}
